d7c1b95b9a0221acbca61b668dff09b9acf488c3
olvneto <olvneto@yahoo.com.br>
2023-01-18 12:43:51 -0300

    Aula sobre testes

---
+++ src/components/PostCard/Post.spec.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { PostCard } from '.';
+import { postCardMock } from './mock';
+
+const mock = postCardMock;
+
+describe('<PostCard />', () => {
+  it('should render PostCard correctly', () => {
+    render(<PostCard {...mock} />);
+
+    expect(screen.getByRole('img', { name: /title 1/i })).toHaveAttribute('src', 'img/img.png');
+    expect(screen.getByRole('heading', { name: 'title 1' })).toBeInTheDocument();
+    expect(screen.getByText('body 1')).toBeInTheDocument();
+  });
+
+  it('should match snapshot', () => {
+    const { container } = render(<PostCard {...mock} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,13 +1,11 @@
 import './styles.css'
 
-export const TextInput = ({ searchValue, handleChange }) => {
-  return (
-    <input
-      className="text-input"
-      onChange={handleChange}
-      value={searchValue}
-      type="search"
-      placeholder="Type your search"
-    />
-  );
-};
+export const TextInput = ({ searchValue, handleChange }) => (
+  <input
+    className="text-input"
+    onChange={handleChange}
+    value={searchValue}
+    type="search"
+    placeholder="Type your search"
+  />
+);
